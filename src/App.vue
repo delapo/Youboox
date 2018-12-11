6da14eb9409f88a08f11e28e1418f2351fb6f95b
delapo_a <delapo_a@etna-alternance.net>
2018-12-11 16:06:40 +0100

    middle-image.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <template>
-  <div id="app">
-    <menu-gauche></menu-gauche>
-    <menu-droite></menu-droite>
-    <drag-drop></drag-drop>
-    <router-view/>
-  </div>
+    <div id="app">
+        <menu-gauche></menu-gauche>
+        <menu-droite></menu-droite>
+        <drag-drop></drag-drop>
+        <middle-image></middle-image>
+        <router-view/>
+    </div>
 </template>
 
 <script>
 import MenuGauche from '@/components/MenuGauche'
 import MenuDroite from '@/components/MenuDroite'
 import DragDrop from '@/components/DragDrop'
+import MiddleImage from '@/components/MiddleImage'
+
 export default {
   name: 'App',
   components: {
     'menu-gauche': MenuGauche,
     'menu-droite': MenuDroite,
+    'middle-image': MiddleImage,
     'drag-drop': DragDrop
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/MiddleImage.vue
@@ -0,0 +1,78 @@
+<template>
+    <div id="middleImage">
+        <section id="prev">
+            <img src="https://www.dsbrowser.com/documentation/images/8/8d/Fleche-droite.png">
+        </section>
+        <section id="BD_show">
+        </section>
+        <section id="next">
+            <img src="https://www.dsbrowser.com/documentation/images/8/8d/Fleche-droite.png">
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'middle-image',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+    #next {
+        width: 70px;
+        height: 70px;
+        position: absolute;
+        z-index: 1;
+        top: 50%;
+        right: 67%;
+        background: black;
+        border-radius: 50px;
+    }
+
+    #next img {
+        width: 50px;
+        height: 50px;
+        position: absolute;
+        z-index: 1;
+        top: 10px;
+        left: 10px;
+        border-radius: 35px;
+        transform: scale(-1, 1);
+    }
+
+    #prev {
+        width: 70px;
+        height: 70px;
+        position: absolute;
+        z-index: 1;
+        top: 50%;
+        left: 67%;
+        background: black;
+        border-radius: 50px;
+    }
+
+    #prev img {
+        width: 50px;
+        height: 50px;
+        position: absolute;
+        z-index: 1;
+        top: 10px;
+        left: 10px;
+        background: black;
+        border-radius: 35px;
+    }
+
+        #BD_show img {
+        top: 0;
+        bottom: 0;
+        width: 50%;
+        height: 100%;
+        margin-left: 24.5%;
+        position: absolute;
+    }
+</style>
