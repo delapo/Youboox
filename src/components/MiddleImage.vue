cf391cb51ba828f8ac27974524fa4fbd04095db2
courcoux <courco_a@etna-alternance.net>
2018-12-13 10:25:49 +0100

    display delete case avec .json

--- src/components/MiddleImage.vue
+++ src/components/MiddleImage.vue
@@ -20,6 +20,71 @@ export default {
     }
   }
 }
+
+var json = '{  "regions_of_interest": [\n' +
+    '      {\n' +
+    '        "x": 322,\n' +
+    '        "y": 84,\n' +
+    '        "width": 464,\n' +
+    '        "height": 462\n' +
+    '      },\n' +
+    '    {\n' +
+    '      "x": 786,\n' +
+    '      "y": 84,\n' +
+    '      "width": 517,\n' +
+    '      "height": 462\n' +
+    '    },\n' +
+    '    {\n' +
+    '      "x": 322,\n' +
+    '      "y": 546,\n' +
+    '      "width": 309,\n' +
+    '      "height": 572\n' +
+    '    },\n' +
+    '    {\n' +
+    '      "x": 631,\n' +
+    '      "y": 546,\n' +
+    '      "width": 323,\n' +
+    '      "height": 572\n' +
+    '    },\n' +
+    '    {\n' +
+    '      "x": 954,\n' +
+    '      "y": 546,\n' +
+    '      "width": 349,\n' +
+    '      "height": 541\n' +
+    '    },\n' +
+    '    {\n' +
+    '      "x": 760,\n' +
+    '      "y": 1087,\n' +
+    '      "width": 543,\n' +
+    '      "height": 437\n' +
+    '    },\n' +
+    '    {\n' +
+    '      "x": 322,\n' +
+    '      "y": 1118,\n' +
+    '      "width": 438,\n' +
+    '      "height": 406\n' +
+    '    }\n' +
+    '  ]\n' +
+    '}';
+ /*
+  obj = JSON.parse(json);
+for (z = 0; z <= obj.regions_of_interest.length; z++)
+{
+    console.log("wsh");
+    var newdiv = document.createElement("div");
+
+    newdiv.setAttribute('id', 'BD_show');
+    newdiv.setAttribute('class', 'bd');
+    /* newdiv["number"] = z;*/
+  /*  newdiv.style.marginLeft = obj.regions_of_interest[z].x + 'px';
+    newdiv.style.marginTop = obj.regions_of_interest[z].y + 'px';
+    newdiv.style.width = obj.regions_of_interest[z].width + 'px';
+    newdiv.style.height = obj.regions_of_interest[z].height + 'px';
+    newdiv.style.border = "thick solid #0000FF";
+    var select = document.getElementById('demo1');
+    select.appendChild(newdiv);
+}*/
+
 </script>
 
 <style scoped>
@@ -73,6 +138,9 @@ export default {
         width: 50%;
         height: 100%;
         margin-left: 24.5%;
+        position: absolute;
+    }
+    .bd {
         position: absolute;
     }
 </style>
