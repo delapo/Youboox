e4cb64312f521da63f6e6ccd6da5b9a09046ca81
Matthieu <matthieu.vigier1@gmail.com>
2018-12-20 23:38:24 +0100

    laché de div + zindex de merde

--- src/components/MenuDroite.vue
+++ src/components/MenuDroite.vue
@@ -5,11 +5,11 @@
             <div id="save_text" v-show="showSave === e"><p>Sauvegarder</p></div>
         </section>
         <section id="_add" ref="add" v-for="e in 1" :key="e.id" @mouseover="showAdd = e" @mouseout="showAdd = null">
-            <img v-bind:class="add_selected" id="imgAdd" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaWv7Z9KZToWdMyUCma8E1jAdzkXd27WIVJ-QuJMurz7s7gLUX_g">
+            <img v-bind:class="add_selected" v-on:click="check(1)" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaWv7Z9KZToWdMyUCma8E1jAdzkXd27WIVJ-QuJMurz7s7gLUX_g">
             <div id="add_text" v-show="showAdd === e"><p>Ajouter une case</p></div>
         </section>
         <section id="del" ref="del" v-for="e in 1" :key="e.id" @mouseover="showRm = e" @mouseout="showRm = null">
-            <img v-bind:class="delete_selected" v-on:click="check(2)" id="imgDel" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaWv7Z9KZToWdMyUCma8E1jAdzkXd27WIVJ-QuJMurz7s7gLUX_g">
+            <img v-bind:class="delete_selected" v-on:click="check(2)" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaWv7Z9KZToWdMyUCma8E1jAdzkXd27WIVJ-QuJMurz7s7gLUX_g">
             <div id="rm_text" v-show="showRm === e">Supprimer une case</div>
         </section>
         <section id="edit" ref="edit" v-for="e in 1" :key="e.id" @mouseover="showEdit = e" @mouseout="showEdit = null">
@@ -61,10 +61,10 @@ export default {
         switch (int) {
           case 1:
             this.selected = 1
-            this.add_selected = 'not selected'
+            this.add_selected = 'tool_selected'
             console.log('add')
             this.delete_selected = 'not selected'
-            this.edit_selected = 'tool_selected'
+            this.edit_selected = 'not selected'
             this.move_selected = 'not selected'
             break
           case 2:
